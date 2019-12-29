d0689e2590aaa9aaec5773f4b376ae1efdaf1cba
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2019-12-29 02:06:19 -0300

    Final fixes, CRUD working 100%

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-npm install axios
-
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -4,9 +4,9 @@ import axios from 'axios';
 
 const Todo = props => (
   <tr>
-    <td>{props.todo.todo_description}</td>
-    <td>{props.todo.todo_responsible}</td>
-    <td>{props.todo.todo_priority}</td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+    <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
     <td>
       <Link to={`/edit/${props.todo._id}`}> Edit</Link>
     </td>
@@ -26,7 +26,17 @@ export default class TodosList extends Component {
         })
           .catch(function(err){
             console.log(err);
-          });        
+          });
+  }
+
+  componentDidUpdate(){
+    axios.get('http://localhost:4000/todos')
+        .then(response => {
+          this.setState({ todos : response.data });
+        })
+          .catch(function(err){
+            console.log(err);
+          });
   }
 
   todoList(){
